89c8d2681ab4398c1fb18d3d9d8319e705e18f86
Damien Stamates <thedoubledimensions@gmail.com>
2017-08-30 14:53:22 -0500

    Testing grounds added
    Added a testing grounds to confirm a working version of an anagram check by using an anonymous function. (function isn't returning any value)

--- anagramCheck.js
+++ anagramCheck.js
@@ -4,3 +4,12 @@
 ////    Summary: This method can be integrated into    ////
 ////   your code to check if two words are anagrams.   ////
 ///////////////////////////////////////////////////////////
+
+
+// Testing grounds
+
+((a,b)=>{
+    var y = a.split('').sort().join(''), z = b.split('').sort().join('');
+    console.log(z === y ? a + ' and ' + b + ' are anagrams' :
+        a + ' and ' + b + ' are not anagrams');
+})('god','dog');
